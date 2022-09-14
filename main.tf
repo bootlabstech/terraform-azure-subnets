9663cc3da48bb5ea405f544b3935a56881ea635c
Naveen-manokaran <naveen.manokaran@bootlabstech.com>
2022-09-14 14:02:31 +0530

    rewriting the module

--- main.tf
+++ main.tf
@@ -1,45 +1,6 @@
-resource "azurerm_resource_group" "resource_group" {
-  name     = var.resource_name
-  location = var.location
-}
-
-resource "azurerm_management_lock" "rglock" {
-  name       = "resource-group-level"
-  scope      = azurerm_resource_group.resource_group.id
-  lock_level = "ReadOnly"
-  notes      = "This Resource Group is Read-Only"
-}
-
-resource "azurerm_network_security_group" "nsg" {
-  name                = var.nsg_name
-  location            = var.resource_name
-  resource_group_name = var.resource_group_name
-  dynamic "security_rule" {
-    for_each = var.nsg_rules
-    content {
-      name                       = security_rule.value["name"]
-      priority                   = security_rule.value["priority"]
-      direction                  = security_rule.value["direction"]
-      access                     = security_rule.value["access"]
-      protocol                   = security_rule.value["protocol"]
-      source_port_range          = security_rule.value["source_port_range"]
-      destination_port_range     = security_rule.value["destination_port_range"]
-      source_address_prefix      = security_rule.value["source_address_prefix"]
-      destination_address_prefix = security_rule.value["destination_address_prefix"]
-    }
-  }
-}
-
-resource "azurerm_virtual_network" "virtual_network" {
-  name                = var.virtual_network_name
-  address_space       = var.virtual_address_space 
-  location            = azurerm_resource_group.resource_group.location
-  resource_group_name = azurerm_resource_group.resource_group.name
-}
-
 resource "azurerm_subnet" "subnet" {
   name                 = var.subnet_name
-  resource_group_name  = azurerm_resource_group.resource_group.name
-  virtual_network_name = azurerm_virtual_network.virtual_network.name
-  address_prefixes     = var.subnet_address_space
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = var.network
+  address_prefixes     = var.subnet_cidr_ranges
 }
--- variables.tf
+++ variables.tf
@@ -1,56 +1,19 @@
-//resource_group
-variable "resource_name" {
-  type        = string
-  description = "The Name which should be used for this Resource Group. Changing this forces a new Resource Group to be created."
-}
-variable "location" {
-  type        = string
-  description = "The Azure Region where the Resource Group should exist. Changing this forces a new Resource Group to be created."
-}
-
-//vn
-variable "virtual_network_name" {
-  description = "The name of the virtual network to which to attach the subnet. Changing this forces a new resource to be created."
-  type        = string
-}
-
-variable "virtual_address_space" {
-  description = "The address prefixes to use for the subnet."
-  type        = list
-}
-
-//subnet
-
 variable "subnet_name" {
   description = "The name of the subnet. Changing this forces a new resource to be created."
   type        = string
 }
 
-variable "subnet_address_space" {
-  description = "The address prefixes to use for the subnet."
-  type        = list
-}
-
-//nsg
-
-variable "nsg_name" {
+variable "resource_group_name" {
   type        = string
-  description = "name of the azurerm_network_security_group"
-}
-variable "nsg_rules" {
-  type = list(object({
-    name                       = string
-    priority                   = number
-    direction                  = string
-    access                     = string
-    protocol                   = string
-    source_port_range          = string
-    destination_port_range     = string
-    source_address_prefix      = string
-    destination_address_prefix = string
-  }))
-  description = "values for each NSG rule"
+  description = "The Name which should be used for this Resource Group. Changing this forces a new Resource Group to be created."
 }
 
+variable "network" {
+  description = "The name of the virtual network to which to attach the subnet. Changing this forces a new resource to be created."
+  type        = string
+}
 
-
+variable "subnet_cidr_ranges" {
+  type = list(string)
+  description = "The address prefixes to use for the subnet."
+}
